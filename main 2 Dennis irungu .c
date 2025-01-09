6afb7ddb50d7c796603ef8154a4c216b110a1526
Irosh6836 <iroshdennis896@gmail.com>
2025-01-09 10:34:39 +0300

    Add files via upload

---
+++ main 2 Dennis irungu .c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+
+    printf("5:");
+    scanf("%d",&a);
+
+    printf("7:");
+    scanf("%d",&b);
+
+    printf("9:");
+    scanf("%d",&c);
+
+    printf("%d + %d + %d = %d/n",a,b,c);
+
+    return 0;
+}
